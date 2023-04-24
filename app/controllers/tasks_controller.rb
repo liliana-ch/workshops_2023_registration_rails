f760e8817bd3bb6153bdb8c351f8fcf32b1e005c
liliana-ch <liliana.cholody@gmail.com>
2023-04-24 18:18:51 +0200

    Task new poprawione, wyświetlają się dynamicznie numery, dodana kolumna deadline

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -11,7 +11,7 @@ class TasksController < ApplicationController
 
   def create
     @task = Task.new(task_params)
-
+    @task.save
     if @task.save
       redirect_to tasks_url, notice: 'Task successfully created.'
     else
@@ -31,6 +31,6 @@ class TasksController < ApplicationController
   end
 
   def task_params
-    params.require(:task).permit(:description)
+    params.require(:task).permit(:title, :description)
   end
 end
