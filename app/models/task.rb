0b6767dfd80ce77ac326f41281a234ec2880a3ef
liliana-ch <liliana.cholody@gmail.com>
2023-05-02 12:29:55 +0200

    skasowana walidacja deadlinu

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,10 +1,3 @@
 class Task < ApplicationRecord
   validates :title, presence: true
-  validates :deadline, presence: true
-  validate :check_deadline_possibility, if: :deadline_changed?
-
-  def check_deadline_possibility
-    return if deadline >= Time.zone.today
-    errors.add(:deadline, 'must not be before today')
-  end
 end
